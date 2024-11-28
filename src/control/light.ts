dd5da61b0ab802c999515ee563ed10e854c49fdd
108yen <kazuking.1911@gmail.com>
2024-11-28 23:28:19 +0900

    chore: update turn off light trigger

--- src/control/light.ts
+++ src/control/light.ts
@@ -10,6 +10,8 @@ export async function lightControl({
   deviceConnected,
   midnight,
 }: Props) {
+  const deviceDisconnectCount = store.get("deviceDisconnectCount") ?? -1
+
   if (data.il.val < TRIGGER.light && daytime && deviceConnected) {
     await cloud.sendSignal(SIGNAL_ID.toggle_light)
 
@@ -17,7 +19,7 @@ export async function lightControl({
     log(`Turn on light. brightness: ${data.il.val}`, "INFO")
   }
 
-  if (data.il.val > TRIGGER.light && midnight && !deviceConnected) {
+  if (data.il.val > TRIGGER.light && midnight && deviceDisconnectCount > 5) {
     await cloud.sendSignal(SIGNAL_ID.toggle_light)
 
     store.set("light", 0)
@@ -36,5 +38,17 @@ export async function lightControl({
       `Update the light parameter to 1 because the light is expected to be on. brightness: ${data.il.val}`,
       "DEBUG",
     )
+  }
+
+  if (!deviceConnected) {
+    if (deviceDisconnectCount == -1) {
+      store.set("deviceDisconnectCount", 0)
+    } else if (deviceDisconnectCount > 5) {
+      store.set("deviceDisconnectCount", 0)
+    } else {
+      store.set("deviceDisconnectCount", deviceDisconnectCount + 1)
+    }
+  } else {
+    store.set("deviceDisconnectCount", 0)
   }
 }
